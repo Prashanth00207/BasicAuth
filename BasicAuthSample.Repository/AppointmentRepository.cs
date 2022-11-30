a1ff4c9fe0b3bd23c71587afeb8b4d105f4be218
Prashanth00207 <118964640+Prashanth00207@users.noreply.github.com>
2022-11-30 16:54:31 +0530

    Add files via upload
    First commit

---
+++ BasicAuthSample.Repository/AppointmentRepository.cs
@@ -0,0 +1,36 @@
+﻿using BasicAuthSample.Repository.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicAuthSample.Repository
+{
+    public class AppointmentRepository
+    {
+        public static List<Appointment> GetAppointments()
+        {
+            HospitalMgmtContext db = new HospitalMgmtContext();
+            return db.Appointments.Include(a=>a.User).Include(a=>a.Doctor).Include(a=>a.CreatedByNavigation).Include(a=>a.ModifiedByNavigation).ToList();
+        }
+        public static Appointment Create(Appointment appointment)
+        {
+            HospitalMgmtContext db = new HospitalMgmtContext();
+            db.Appointments.Add(appointment);
+            db.SaveChanges();
+            return appointment;
+        }
+        public static List<Appointment> GetAppointmentsByDoctorId(int id)
+        {
+            HospitalMgmtContext db = new HospitalMgmtContext();
+            return db.Appointments.Include(a => a.User).Include(a => a.Doctor).Include(a => a.CreatedByNavigation).Include(a => a.ModifiedByNavigation).Where(a=>a.DoctorId==id).ToList();
+        }
+        public static List<Appointment> GetAppointmentsByPatientId(int id)
+        {
+            HospitalMgmtContext db = new HospitalMgmtContext();
+            return db.Appointments.Include(a => a.User).Include(a => a.Doctor).Include(a => a.CreatedByNavigation).Include(a => a.ModifiedByNavigation).Where(a=>a.UserId==id).ToList();
+        }
+    }
+}
---
+++ BasicAuthSample.Repository/LoginRepository.cs
@@ -0,0 +1,49 @@
+﻿using BasicAuthSample.Repository.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicAuthSample.Repository
+{
+    public class LoginRepository
+    {
+        public static Login GetUserByUserName(string userName)
+        {
+            HospitalMgmtContext db = new HospitalMgmtContext();
+            return db.Logins.Include(l => l.UserRoles).ThenInclude(ur => ur.Role).Where(u => u.UserName == userName).FirstOrDefault();
+        }
+        public static List<Login> GetUsers(UserTypeEnum type)
+        {
+            HospitalMgmtContext db = new HospitalMgmtContext();
+            return db.Logins.Where(u => u.UserTypeId == (int)type).ToList();
+        }
+        public static Login Register(Login login)
+        {
+            HospitalMgmtContext db = new HospitalMgmtContext();
+            db.Logins.Add(login);
+            db.SaveChanges();
+            return login;
+
+        }
+
+
+        public static Login IsLoginValid(Login login)
+        {
+            HospitalMgmtContext db = new HospitalMgmtContext();
+            // Login dbRecord = db.Logins.Where(loginRecord => (loginRecord.UserName == login.UserName) && (loginRecord.Password == login.Password)).FirstOrDefault();
+            Login dbRecord = db.Logins.Include(l => l.UserRoles).ThenInclude(ur => ur.Role).Where(loginRecord => (loginRecord.UserName == login.UserName) && (loginRecord.Password == login.Password)).FirstOrDefault();
+
+            if (dbRecord != null)
+            {
+                return dbRecord;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
